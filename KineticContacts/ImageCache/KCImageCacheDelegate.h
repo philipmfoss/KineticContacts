25e26d9d7b98c0e5689e23338520350615f8374f
philipmfoss <philip@Philips-MacBook-Air.local>
2015-01-07 12:35:40 -0500

    Updated comments.

--- KineticContacts/ImageCache/KCImageCacheDelegate.h
+++ KineticContacts/ImageCache/KCImageCacheDelegate.h
@@ -2,9 +2,15 @@
 
 #import <Foundation/Foundation.h>
 
+// Delegate protocol for KCImageCache.
+// Users of KCImageCache should implement this protocol.
 @protocol KCImageCacheDelegate <NSObject>
 
+// Passes back an image for a given URL on successful download.
+// User can call getImage on KCImageCache to retrieve the image at the URL.
 - (void)loadedImage:(UIImage*)image forUrl:(NSURL*)url;
+
+// Called when cleared method executes successfully.
 - (void)cleared;
 
 @end
