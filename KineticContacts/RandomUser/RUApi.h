3f36e676e5dbc2309a816a5c0135025fbe03100a
philipmfoss <philip@Philips-MacBook-Air.local>
2015-01-07 12:30:51 -0500

    Updated comments.

--- KineticContacts/RandomUser/RUApi.h
+++ KineticContacts/RandomUser/RUApi.h
@@ -11,10 +11,29 @@
 #import "RULocation.h"
 #import "RUPicture.h"
 
+// Encapsulates RandomUser Web service.
+// See https://randomuser.me
+// Only version 0.4.1 of the RadomUser API is supported.
+// Not all fields are available. See RUUser class for the fields that are available.
 @interface RUApi : NSObject
 
+// Rerieves a single user with a random seed from RandomUser web service.
+// If successful passes back an array with a single RUResult object through the delegate.
+// Passes back an error through the delegate on either a system error or a web-service error.
+// See RUApiDelegate
 - (void)getRandomUserWithDelegate:(id<RUApiDelegate>)delegate;
+
+// Rerieves the specified number of users with a random seed from RandomUser web service.
+// If successful passes back an array of RUResult objects through the delegate.
+// Passes back an error through the delegate on either a system error or a web-service error.
+// See RUApiDelegate
 - (void)getRandomUsers:(NSInteger)count withDelegate:(id<RUApiDelegate>)delegate;
+
+// Handles the response from the RandomUser service.
+// Only application/json MIME type is supported.
+// If successful passes back an array of RUResult objects through the delegate.
+// Passes back an error through the delegate on either a system error or a web-service error.
+// See RUApiDelegate
 - (void)handleResponse:(NSString*)mimeType withData:(NSData*)data withError:(NSError*)error withDelegate:(id<RUApiDelegate>)delegate;
 
 @end
--- KineticContacts/RandomUser/RUApiDelegate.h
+++ KineticContacts/RandomUser/RUApiDelegate.h
@@ -4,10 +4,21 @@
 
 #import <Foundation/Foundation.h>
 
+// Delegate protocol for the RUApi class.
+// The RUApi object container should implement this interface.
 @protocol RUApiDelegate <NSObject>
 
+// Passes back an array of RUResult objects on a successful call to RandomUser web-service.
+// See https://randomuser.me
+// Only version 0.4.1 of the RadomUser API is supported.
+// Not all fields are available. See RUUser class for the fields that are available.
 - (void)didGetRandomUserResults:(NSArray*)results;
+
+// Passes back an error through the delegate on either a system error.
 - (void)failedGetRandomUserResponseWithError:(NSError*)error;
+
+// Passes back an error string through the delegate on either a system error.
+// See https://randomuser.me documentation concerning errors.
 - (void)failedGetRandomUserResponseWithApiError:(NSString*)error;
 
 @end
